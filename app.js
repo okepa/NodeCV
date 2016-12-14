aa107f4c6286de7605b01aa5d2f022900b0fe1d5
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2016-12-14 11:41:58 +0000

    changed email for email file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes");
 const contactRoutes = require("./routes/contact");
-const app = express();
+const layout = require('express-ejs-layouts');
 
 //View Engine
 app.set('view engine' , 'ejs');
@@ -11,6 +12,7 @@ app.set('view engine' , 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(layout);
 app.use(routes);
 app.use(contactRoutes);
 
